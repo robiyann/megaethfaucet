d39942cb462eb31627ab26c55695756147e5698e
robiyann <jamalbulkin@gmail.com>
2025-04-25 09:39:59 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+echo "📦 Update & upgrade sistem..."
+sudo apt update && sudo apt upgrade -y
+
+echo "📥 Install curl dan Node.js LTS..."
+sudo apt install curl -y
+curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
+sudo apt install nodejs -y
+sudo apt install npm -y
+
+echo "🧪 Cek versi Node.js dan npm..."
+node -v
+npm -v
+
+echo "🧰 Inisialisasi project npm dan install dependensi..."
+npm init -y
+npm install axios https-proxy-agent
+
+echo " SEMUA SUDAH TERINSTALL YA TOD"
